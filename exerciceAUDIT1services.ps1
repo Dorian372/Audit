550c1a7b22f068c2e699e1a9f1328ee70be45841
Dorian372 <81350804+Dorian372@users.noreply.github.com>
2021-03-25 09:53:19 +0100

    Add files via upload

---
+++ exerciceAUDIT1services.ps1
@@ -0,0 +1,21 @@
+﻿# 
+# Auteur : Vacher Dorian
+# 
+#
+
+########### Début du script ###########
+
+If(!(Test-Path -path "C:\Powershell\auditServices.txt"))
+{
+    New-Item "C:\Powershell\auditServices.txt" -ItemType File
+}
+
+$DateNow = Get-Date -Format "dd/MM/yyyy"
+ 
+Add-Content -path "C:\Powershell\auditServices.txt" -value " ############## $DateNow ##############"
+
+Get-Service -Name DhcpServer >> C:\Powershell\auditServices.txt
+
+Get-Service -Name DNS >> C:\Powershell\auditServices.txt
+
+get
