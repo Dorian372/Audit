b33e35b6e3991d408253456a21426ed203a7d089
Vacher Dorian <you@example.com>
2021-03-25 08:33:55 +0100

    first commit

--- exerciceAudit.ps1
+++ exerciceAudit.ps1
@@ -1,9 +1,8 @@
 ﻿# 
 # Auteur : Vacher Dorian
 # 
-#fdsf dWOafdjsklfsd 
+#
 
-sdqd fds  fs gfd  fgsdf ds ds gfd 
 ########### Début du script ###########
 
 Add-Type -AssemblyName System.Windows.Forms
